228ff7e494e59c1decd8f4c921c96debca2da31f
Gaël Luisier <luisier.gael@gmail.com>
2024-07-29 10:47:30 +0200

    Restrict access to active users

---
+++ src/Security/ActiveUserChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ActiveUserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+
+        if (!$user->isActive()) {
+            throw new CustomUserMessageAccountStatusException('Inactive account');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+    }
+}
