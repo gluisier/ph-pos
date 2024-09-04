a1170e79ff7bee805eee678ac7b50abe7d4823dd
Gaël Luisier <luisier.gael@gmail.com>
2024-09-04 22:00:17 +0200

    Add shortcuts to base page
    And make price list public

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -15,10 +15,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/item', name: 'item_')]
+#[Route(name: 'item_')]
 class ItemController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
+    #[Route('/item/', name: 'index', methods: ['GET'])]
     public function index(ItemRepository $itemRepository): Response
     {
         return $this->render('item/index.html.twig', [
@@ -26,7 +26,7 @@ class ItemController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    #[Route('/item/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $item = new Item();
@@ -46,7 +46,7 @@ class ItemController extends AbstractController
         ]);
     }
 
-    #[Route('/{id<\d+>}', name: 'show', methods: ['GET'])]
+    #[Route('/item/{id<\d+>}', name: 'show', methods: ['GET'])]
     public function show(Item $item): Response
     {
         return $this->render('item/show.html.twig', [
@@ -54,7 +54,7 @@ class ItemController extends AbstractController
         ]);
     }
 
-    #[Route('/{id<\d+>}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    #[Route('/item/{id<\d+>}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Item $item, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ItemType::class, $item);
@@ -72,7 +72,7 @@ class ItemController extends AbstractController
         ]);
     }
 
-    #[Route('/{id<\d+>}', name: 'delete', methods: ['POST'])]
+    #[Route('/item/{id<\d+>}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Item $item, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$item->getId(), $request->request->get('_token'))) {
@@ -83,7 +83,7 @@ class ItemController extends AbstractController
         return $this->redirectToRoute('item_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/event', name: 'event', methods: ["GET", "POST"])]
+    #[Route('/item/event', name: 'event', methods: ["GET", "POST"])]
     public function event(Request $request, FormFactoryInterface $formFactory, EntityManagerInterface $entityManager)
     {
         $items = $entityManager->getRepository(Item::class)->findBy(['separatelySellable' => true], ['category' => 'ASC', 'position' => 'ASC']);
