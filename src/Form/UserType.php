1d43f7f9276ff5d7fe901890661e2fae3a3c5b9e
Gaël Luisier <luisier.gael@gmail.com>
2025-06-21 23:53:50 +0200

    Printer field for user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormRendererInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -18,6 +19,20 @@ class UserType extends AbstractType
             ])
             ->add('active', null, [
                 'label_format' => 'app.fields.user.%name%.label',
+            ])
+            ->add('printer', null, [
+                'query_builder' => function (\App\Repository\PrinterRepository $repo) {
+                    $qb = $repo->createQueryBuilder('p');
+                    $qb ->where($qb->expr()->neq('p.status', ':status'))
+                        ->setParameter(':status', \App\Config\Printer\Status::STORED)
+                    ;
+
+                    return $qb;
+                },
+                'choice_label' => function (\App\Entity\Printer $choice) {
+                    return $choice->getManufacturer()->value . ' ' . $choice->getModel();
+                },
+                'label_format' => 'app.fields.user.%name%.label.short',
             ])
             ->add('roles', CollectionType::class, [
                 'label_format' => 'app.fields.user.%name%.label',
