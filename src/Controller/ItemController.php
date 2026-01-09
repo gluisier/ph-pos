00e78b3e04c2a1dbbfa787cef34b7027c961adb1
Gaël Luisier <luisier.gael@gmail.com>
2026-01-09 14:20:38 +0100

    Fix namespace deprecation

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(name: 'item_')]
 class ItemController extends AbstractController
